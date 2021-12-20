64bb93253fb18542856265b4d7514ee79ab002ff
JkAsym <86189843+JkAsym@users.noreply.github.com>
2021-12-20 08:43:44 +0300

    Delete btn-change-theme.js

--- btn-change-theme.js
+++
@@ -1,7 +0,0 @@
-let btn = document.querySelector('.theme-btn');
-let page = document.querySelector('.page');
-
-btn.onclick = function () {
-    page.classList.toggle('dark-theme');
-    page.classList.toggle('light-theme');
-};
